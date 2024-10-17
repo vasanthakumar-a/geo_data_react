c2065d75cb3840459f7da3d918bba7a5d79c79f9
vasanthakumar-a <vasanthakumara117@gmail.com>
2024-10-18 01:32:29 +0530

    Map edit layer added

--- src/components/Dashboard/Map.js
+++ src/components/Dashboard/Map.js
@@ -1,46 +1,96 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
 import "leaflet-draw/dist/leaflet.draw.css";
 import L from "leaflet";
 import "leaflet-draw";
 import "leaflet/dist/leaflet.css";
 
-const DrawControl = () => {
+const DrawControl = ({ onSave, savedShapes, setSavedShapes }) => {
   const map = useMap();
+  const [drawnItems, setDrawnItems] = useState(new L.FeatureGroup());
 
   useEffect(() => {
-    const drawnItems = new L.FeatureGroup();
     map.addLayer(drawnItems);
 
     const drawControl = new L.Control.Draw({
       edit: {
         featureGroup: drawnItems,
+        remove: true, 
       },
       draw: {
         polygon: true,
         rectangle: true,
-        circle: false,
-        marker: false,
-        polyline: false,
+        circle: true,
+        marker: true,
+        polyline: true,
       },
     });
     map.addControl(drawControl);
 
+    if (savedShapes) {
+      const loadedData = L.geoJSON(savedShapes);
+      loadedData.eachLayer((layer) => {
+          drawnItems.addLayer(layer);
+      });
+    }
+
     map.on(L.Draw.Event.CREATED, (event) => {
       const layer = event.layer;
+      drawnItems.clearLayers();
       drawnItems.addLayer(layer);
+      setDrawnItems(drawnItems);
     });
 
     return () => {
       map.off(L.Draw.Event.CREATED);
       map.removeControl(drawControl);
     };
-  }, [map]);
+  }, [map, drawnItems, savedShapes]);
 
-  return null;
+  const handleSave = () => {
+    const savedShapes = drawnItems.toGeoJSON();
+    onSave(savedShapes);
+  };
+
+  return (
+    <>
+      <button
+        onClick={handleSave}
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          zIndex: 1000,
+        }}
+      >
+        Save Map
+      </button>
+    </>
+  );
 };
 
-const Map = ({ geoJsonData }) => {
+const Map = () => {
+  const [savedShapes, setSavedShapes] = useState(null);
+
+  useEffect(() => {
+      const savedData = localStorage.getItem('savedShapes');
+      if (savedData) {
+          setSavedShapes(JSON.parse(savedData));
+      }
+  }, []);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("savedShapes");
+    if (savedData) {
+      setSavedShapes(JSON.parse(savedData));
+      setSavedShapes(JSON.parse(savedData));
+    }
+  }, []);
+
+  const saveShapes = (data) => {
+    localStorage.setItem("savedShapes", JSON.stringify(data));
+  };
+
   return (
     <MapContainer
       center={[51.505, -0.09]}
@@ -51,8 +101,7 @@ const Map = ({ geoJsonData }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {geoJsonData && <GeoJSON data={geoJsonData} />}
-      <DrawControl />
+      <DrawControl onSave={saveShapes} savedShapes={savedShapes} setSavedShapes={setSavedShapes} />
     </MapContainer>
   );
 };
