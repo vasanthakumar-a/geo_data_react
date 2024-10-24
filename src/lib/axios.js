cd5526335edad3dab3d4099d73a64db55359e658
vasanthakumar-a <vasanthakumara117@gmail.com>
2024-10-24 20:22:14 +0530

    added axios link

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://geo-data-api.onrender.com',
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
